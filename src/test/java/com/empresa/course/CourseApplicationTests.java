1053b0e8f7500988a2174e2807b49afb7e6a439d
Pedro Nunes <pedrocardoso0018@gmail.com>
2022-11-10 17:27:57 -0300

    project created

---
+++ src/test/java/com/empresa/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
