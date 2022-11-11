a7bba5297d8e8d1ea47705304f31b2fc65531038
Pedro Nunes <pedrocardoso0018@gmail.com>
2022-11-11 16:15:13 -0300

    service layer, component registration

--- src/main/java/com/empresa/course/repositories/UserRepository.java
+++ src/main/java/com/empresa/course/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.empresa.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
+
 
 }
